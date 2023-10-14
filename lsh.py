5810dcca85d0c6048ca7323671d5a8fa5f399554
Karthick47v2 <karthicksharma11@gmail.com>
2023-10-14 08:27:01 +0530

    update

--- lsh.py
+++ lsh.py
@@ -1,66 +1,89 @@
-
-
 import numpy as np
 from collections import defaultdict
-import heapq
 import ctypes
-import itertools
-import math
 
 np.random.seed(42)
 
-random_generator = ctypes.CDLL('./random_generator.so')
+random_generator = ctypes.CDLL('./rand_gen.so')
 random_generator.generate_random_numbers.argtypes = [
-    ctypes.c_int, 
+    ctypes.c_int,
     ctypes.c_int,
     ctypes.c_int,
 ]
-random_generator.generate_random_numbers.restype = ctypes.POINTER(ctypes.c_double)
+random_generator.generate_random_numbers.restype = ctypes.POINTER(
+    ctypes.c_double)
+random_generator.free_rand_ptr.argtypes = [ctypes.POINTER(ctypes.c_double)]
 
-nn_dist = ctypes.CDLL('./nn_dist.so')
+core_dist = ctypes.CDLL('./core_dist.so')
+core_dist.calc_core_dist.argtypes = [
+    ctypes.POINTER(ctypes.POINTER(
+        ctypes.POINTER(ctypes.POINTER(ctypes.c_double)))),
+    ctypes.POINTER(ctypes.POINTER(ctypes.c_int)),
+    ctypes.POINTER(ctypes.c_int),
+    ctypes.c_int,
+    ctypes.c_int,
+    ctypes.POINTER(ctypes.POINTER(ctypes.c_double)),
+    ctypes.c_int,
+    ctypes.c_int,
+    ctypes.c_int]
 
-nn_dist.calculate_core_dist.argtypes = [
-    ctypes.POINTER(ctypes.POINTER(ctypes.POINTER(ctypes.POINTER(ctypes.c_double)))), 
-    ctypes.POINTER(ctypes.POINTER(ctypes.c_int)), 
-    ctypes.POINTER(ctypes.c_int),  
-    ctypes.c_int, 
-    ctypes.c_int,
-    ctypes.POINTER(ctypes.POINTER(ctypes.c_double)), 
-    ctypes.c_int,
-    ctypes.c_int,
-    ctypes.c_int
-]
-
-nn_dist.calculate_core_dist.restype = ctypes.POINTER(ctypes.c_double)
+core_dist.calc_core_dist.restype = ctypes.POINTER(ctypes.c_double)
 
 
 def generate_random_planes(num_hashtables, hash_size, input_dim):
-    random_numbers_ptr = random_generator.generate_random_numbers(ctypes.c_int(num_hashtables), ctypes.c_int(hash_size), ctypes.c_int(input_dim))
-    random_numbers = np.ctypeslib.as_array(random_numbers_ptr, shape=(num_hashtables, hash_size, input_dim))
-    
+    random_numbers_ptr = random_generator.generate_random_numbers(
+        ctypes.c_int(num_hashtables), ctypes.c_int(hash_size), ctypes.c_int(input_dim))
+    random_numbers = np.ctypeslib.as_array(
+        random_numbers_ptr, shape=(num_hashtables, hash_size, input_dim))
+    # random_generator.free_rand_ptr(random_numbers_ptr) ### FREEING RESULT NULL
+
     return random_numbers
+
 
 class LSHash(object):
     def __init__(self, hash_size, input_dim, num_hashtables=1):
         self.hash_size = hash_size
         self.input_dim = input_dim
         self.num_hashtables = num_hashtables
-        # self.uniform_planes = np.random.randn(self.num_hashtables, self.hash_size, self.input_dim)
-        self.uniform_planes = generate_random_planes(self.num_hashtables, self.hash_size, self.input_dim)
-        self.hash_tables = [defaultdict(list) for _ in range(self.num_hashtables)]
-
+        # self.uniform_planes = np.random.randn(
+        #     self.num_hashtables, self.hash_size, self.input_dim)
+        # print(self.uniform_planes)
+        # yep.start('random_check.prof')
+        self.uniform_planes = generate_random_planes(
+            self.num_hashtables, self.hash_size, self.input_dim)
+        self.hash_tables = [defaultdict(list)
+                            for _ in range(self.num_hashtables)]
 
     def add_points(self, input_points):
         self.input_points = input_points
         for i, table in enumerate(self.hash_tables):
             planes = self.uniform_planes[i]
             projections = np.dot(planes, input_points.T)
-            hash_strings = (projections > 0).T
+            hash_strings = [x.tobytes() for x in (projections > 0).T]
 
             for h, point in zip(hash_strings, input_points):
-                table[h.tobytes()].append(point)
+                table[h].append(point)
+            # print(table.values())
 
-    def nn_dist(self, num_results=None):
+            # print(len(table))
+            # print(list(map(len, table.values())))
+
+            # temp = []
+            # keys = []
+            # for k in table.keys():
+            #     if len(table[k]) < 5:
+            #         temp.extend(table[k])
+            #         keys.append(k)
+
+            # table[1] = temp
+
+            # for k in keys:
+            #     del table[k]
+
+            # print(len(table))
+            # print(list(map(len, table.values())))
+
+    def nn_dist(self, num_results=0):
         if num_results == 0:
             return
 
@@ -74,56 +97,56 @@ class LSHash(object):
             num_bins.append(len(temp))
             dict_list.append(temp)
 
+
         max_arr = [v for n in num_elements_ for v in n]
-        max_arr = int(np.percentile(max_arr, 98))
+        max_arr = int(np.percentile(max_arr, 80)) * self.num_hashtables
 
-        # print(temp, np.percentile(temp, 100))
+        # max_arr -= 1
+        # for i in range(6):  # Perform a maximum of 6 iterations for a 32-bit integer
+        #     max_arr |= max_arr >> (2 ** i)
 
-        max_arr -= 1
-        for i in range(6):  # Perform a maximum of 6 iterations for a 32-bit integer
-            max_arr |= max_arr >> (2 ** i)
-
-        max_arr += 1
+        # max_arr += 1
+        # # max_arr = 10
 
         print('dma size', max_arr)
 
         n_list = len(dict_list)
 
-        c_input_data = (ctypes.POINTER(ctypes.POINTER(ctypes.POINTER(ctypes.c_double))) * n_list)()
-        # c_input_data = (ctypes.POINTER(ctypes.POINTER(ctypes.POINTER(ctypes.c_double))) * n_list)()
+
+        c_input_data = (ctypes.POINTER(ctypes.POINTER(
+            ctypes.POINTER(ctypes.c_double))) * n_list)()
         num_inner_lists = (ctypes.c_int * n_list)()
         num_elements = (ctypes.POINTER(ctypes.c_int) * n_list)()
-        
+
         for i, outer_list in enumerate(dict_list):
             num_inner_lists[i] = num_bins[i]
             num_elements[i] = (ctypes.c_int * num_bins[i])()
-            c_input_data[i] = (ctypes.POINTER(ctypes.POINTER(ctypes.c_double)) * num_bins[i])()
-            # c_input_data[i] = (ctypes.POINTER(ctypes.POINTER(ctypes.c_double)) * num_bins[i])()
-            
+            c_input_data[i] = (ctypes.POINTER(
+                ctypes.POINTER(ctypes.c_double)) * num_bins[i])()
+
             for j, inner_list in enumerate(outer_list):
                 num_elements[i][j] = num_elements_[i][j]
-                c_input_data[i][j] = (ctypes.POINTER(ctypes.c_double) * num_elements_[i][j])()
-                # c_input_data[i][j] = (ctypes.POINTER(ctypes.c_double) * num_elements_[i][j])()
-                
+                c_input_data[i][j] = (ctypes.POINTER(
+                    ctypes.c_double) * num_elements_[i][j])()
+
                 for k, arr in enumerate(inner_list):
-                    c_input_data[i][j][k] = arr.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
-                    # c_input_data[i][j][k] = (ctypes.c_double * self.input_dim)()
-                    # for l, a in enumerate(arr):
-                    #     c_input_data[i][j][k][l] = a #ctypes.c_double(a)
+                    c_input_data[i][j][k] = arr.ctypes.data_as(
+                        ctypes.POINTER(ctypes.c_double))
 
         c_array = (ctypes.POINTER(ctypes.c_double) * len(self.input_points))()
         for i, row in enumerate(self.input_points):
             c_array[i] = row.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
 
-########## FIND BEST VALUES FOR MAX_NEIGHBOUR, HASH_TABLE, HYPERPLANE
+# FIND BEST VALUES FOR MAX_NEIGHBOUR, HASH_TABLE, HYPERPLANE
 # Speed depends on max neigh for C dym alloc
-        result_ptr = nn_dist.calculate_core_dist(c_input_data, num_elements, num_inner_lists, ctypes.c_int(n_list), ctypes.c_int(self.input_dim), c_array, ctypes.c_int(len(self.input_points)), ctypes.c_int(max_arr), ctypes.c_int(num_results))
+        result_ptr = core_dist.calc_core_dist(c_input_data, num_elements, num_inner_lists, ctypes.c_int(n_list), ctypes.c_int(
+            self.input_dim), c_array, ctypes.c_int(len(self.input_points)), ctypes.c_int(max_arr), ctypes.c_int(num_results))
 
-        results = np.ctypeslib.as_array(result_ptr, shape=(len(self.input_points),))
+        results = np.ctypeslib.as_array(
+            result_ptr, shape=(len(self.input_points),))
 
         return results
-
-        # print(result)
+        # print(results)
 
         # output = {}
         # candidates = defaultdict(set)
@@ -136,7 +159,8 @@ class LSHash(object):
 
         # for point, neighbours in candidates.items():
         #     p = np.array(point)
-        #     candidates[point] = [(neighbour, LSHash.euclidean_dist_square(p, neighbour)) for neighbour in neighbours]
+        #     candidates[point] = [(neighbour, LSHash.euclidean_dist_square(
+        #         p, neighbour)) for neighbour in neighbours]
 
         #     heap = []
 
@@ -148,9 +172,10 @@ class LSHash(object):
 
         #     output[point] = np.sqrt(-heap[0])
 
-        return output
+        # print(list(output.keys()))
+        # print(list(output.values()))
+        # # return list(output.values())
 
     def euclidean_dist_square(x, y):
         diff = x - y
         return np.dot(diff, diff)
-    
--- train.py
+++ train.py
@@ -11,88 +11,96 @@ import math
 from time import time
 
 if __name__ == '__main__':
+    X, y = datasets.make_blobs(
+        n_samples=10000, n_features=100, centers=10, cluster_std=2.5, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.01, random_state=42)
 
-    for n_samples in [100000, 1000000, 10000000, 100000000]:
-        for n_features in [100]:
-            print()
-            print(n_samples, n_features)
-            X, y = datasets.make_blobs(n_samples=n_samples, n_features=n_features, centers=10, random_state=42)
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01, random_state=42)
+    K = 3
 
-            K = 5
+    s = time()
+    clf = KNeighborsClassifier(n_neighbors=K, algorithm='brute', n_jobs=12)
+    clf.fit(X_train, y_train)
+    distances, indices = clf.kneighbors(X_train)
+    br_distances = distances[:, -1]
+    # print(X_train)
+    print()
+    # print(X_train[indices[:,K-1]])
+    # print(br_distances)
+    t = time()
+    print('sklearn-knn-bruteforce', t-s)
 
-            # s = time()
-            # clf = KNeighborsClassifier(n_neighbors=K, algorithm='brute', n_jobs=12)
-            # clf.fit(X_train, y_train)
-            # distances, indices = clf.kneighbors(X_train)
-            # br_distances = distances[:, -1]
-            # # print(kth_distances)
-            # t = time()
-            # print('sklearn-knn-bruteforce', t-s)
+    # s = time()
+    # clf = KNeighborsClassifier(n_neighbors=K, algorithm='kd_tree', n_jobs=12)
+    # clf.fit(X_train, y_train)
+    # distances, indices = clf.kneighbors(X_train)
+    # kd_distances = distances[:, -1]
+    # # print(kth_distances)
+    # t = time()
 
-            # s = time()
-            # clf = KNeighborsClassifier(n_neighbors=K, algorithm='kd_tree', n_jobs=12)
-            # clf.fit(X_train, y_train)
-            # distances, indices = clf.kneighbors(X_train)
-            # kd_distances = distances[:, -1]
-            # # print(kth_distances)
-            # t = time()
+    # print('sklearn-knn-kdtree', t-s)
 
-            # print('sklearn-knn-kdtree', t-s)
+    # s = time()
+    # clf = KNeighborsClassifier(n_neighbors=K, algorithm='ball_tree', n_jobs=12)
+    # clf.fit(X_train, y_train)
+    # distances, indices = clf.kneighbors(X_train)
+    # ball_distances = distances[:, -1]
+    # # print(kth_distances)
+    # t = time()
 
+    # print('sklearn-knn-balltree', t-s)
 
-            # s = time()
-            # clf = KNeighborsClassifier(n_neighbors=K, algorithm='ball_tree', n_jobs=12)
-            # clf.fit(X_train, y_train)
-            # distances, indices = clf.kneighbors(X_train)
-            # ball_distances = distances[:, -1]
-            # # print(kth_distances)
-            # t = time()
+    # # s = time()
+    # # clusterer = hdbscan.HDBSCAN(min_cluster_size=5)#, min_samples=K)
+    # # clusterer.fit(X_train)
+    # # d = clusterer.labels_
+    # # t = time()
 
-            # print('sklearn-knn-balltree', t-s)
+    # # print('hdbscan', t-s)
 
-            # # s = time()
-            # # clusterer = hdbscan.HDBSCAN(min_cluster_size=5)#, min_samples=K)
-            # # clusterer.fit(X_train)
-            # # d = clusterer.labels_
-            # # t = time()
+    s = time()
+    h = 1 * math.floor(math.log2(len(X_train) / K))
+    n_t = math.ceil(math.log10(len(X_train[1])))
+    n_t = 5 * h + 1   # 2 -> 10-10
 
-            # # print('hdbscan', t-s)
+    # n_t = 100
 
-            s = time()
-            h = math.floor(math.log2(len(X_train) / K))
-            n_t = math.ceil(math.log10(len(X_train[1])))
-            print('hyperplanes', h)
-            print('# tables', n_t)
+    if h == 0:
+        h = 1
+    # if n_t == 1:
+    #     n_t = 10
 
-            lsh = LSHash(h*3, len(X_train[0]), n_t)
-            lsh.add_points(X_train)
-            dist = lsh.nn_dist(K)
-            t = time()
+    print('hyperplanes', h)
+    print('# tables', n_t)
 
-            print('lsh', t-s)
+    lsh = LSHash(h, len(X_train[0]), n_t)  # h*3
+    # lsh = LSHash(1, len(X_train[0]), 10)
+    lsh.add_points(X_train)
+    dist = lsh.nn_dist(K)
+    t = time()
 
-            # # print(dist)
+    print('lsh', t-s)
 
-            # # def func():
-            # #     # lsh = LSHash(1000, len(X_train[0]), 10)
-            # #     # lsh.add_points(X_train)
-            # #     dist = lsh.nn_dist(K)
-                
-                
-            
-            # # cProfile.run('func()')
+    # print(dist)
 
-            # print('lsh', np.mean((np.abs(br_distances - dist)/br_distances) * 100))
-            # print('sk-kd', np.mean((np.abs(br_distances - kd_distances)/br_distances) * 100))
-            # print('sk-ball', np.mean((np.abs(br_distances - ball_distances)/br_distances) * 100))
+    # lsh = LSHash(h, len(X_train[0]), n_t)
+    # lsh.add_points(X_train)
+    # dist = lsh.nn_dist(K)
 
-            # # lsh_close_mask = np.isclose(dist, br_distances, atol=1e-2)
-            # # kd_close_mask = np.isclose(kd_distances, br_distances, atol=1e-2)
-            # # ball_close_mask = np.isclose(ball_distances, br_distances, atol=1e-2)
+    # def func():
+    #     dist = lsh.nn_dist(K)
 
-            # # # print(kth_distances == dist)
-            # # print('kd', np.sum(kd_close_mask) / len(X_train))
-            # # print('ball', np.sum(ball_close_mask) / len(X_train))
-            # # print('lsh', np.sum(lsh_close_mask) / len(X_train))
-            
+    # cProfile.run('func()')
+
+    # print('lsh', np.mean((np.abs(br_distances - dist)/br_distances) * 100))
+    # print('sk-kd', np.mean((np.abs(br_distances - kd_distances)/br_distances) * 100))
+    # print('sk-ball', np.mean((np.abs(br_distances - ball_distances)/br_distances) * 100))
+
+    lsh_close_mask = np.isclose(dist, br_distances, atol=1e-9)
+    # kd_close_mask = np.isclose(kd_distances, br_distances, atol=1e-9)
+    # ball_close_mask = np.isclose(ball_distances, br_distances, atol=1e-9)
+
+    # # print(br_distances == dist)
+    # print('kd', np.sum(kd_close_mask) / len(X_train))
+    # print('ball', np.sum(ball_close_mask) / len(X_train))
+    print('lsh', np.sum(lsh_close_mask) / len(X_train))
